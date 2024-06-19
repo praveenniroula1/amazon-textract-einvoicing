a99a805431c241dfaf4416ccfe1848cad298ed9d
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2024-06-19 18:37:03 +0930

    ready for deployment

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import "dotenv/config"
 import express from "express";
 import cors from "cors";
 import textractRouter from "./src/textractRouter.js"; // Assuming this is your router file
+import path from "path";
 
 const app = express();
 app.use(cors());
@@ -13,17 +14,17 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/v1/textract", textractRouter);
 
 
-// // Serve static assets (React frontend)
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, "/client/build")));
+// Serve static assets (React frontend)
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-// app.use("/", (req,res)=>{
-//   try {
-//     res.sendFile(path.join(__dirname, "/client/build/index.html"))
-//   } catch (error) {
-//     console.log(error)
-//   }
-// })
+app.use("/", (req,res)=>{
+  try {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"))
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 
 app.listen(PORT, () => {
