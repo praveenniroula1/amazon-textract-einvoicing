1910250969b401a00c0217ea1e344f16945d0683
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2024-06-19 20:49:07 +0930

    ready for deployment

--- api/server.js
+++ api/server.js
@@ -1,33 +1,28 @@
-import "dotenv/config"
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import textractRouter from "./src/textractRouter.js"; // Assuming this is your router file
+import textractRouter from "./src/textractRouter.js"; 
 import path from "path";
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
-
 
 // API Endpoint for Textract upload
 app.use("/api/v1/textract", textractRouter);
 
-
 // Serve static assets (React frontend)
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.use("/", (req,res)=>{
-  try {
-    res.sendFile(path.join(__dirname, "/client/build/index.html"))
-  } catch (error) {
-    console.log(error)
-  }
-})
+// Handle React routing, return all requests to React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-  
+  console.log(`Server is running on port ${PORT}`);
+});
